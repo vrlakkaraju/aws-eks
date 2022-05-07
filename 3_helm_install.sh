5792bf057bf2f4fa2aa012b0f57d46acfa223229
vrlakkaraju <vrlakkaraju@gmail.com>
2022-05-07 23:51:30 +0000

    helm install script

---
+++ 3_helm_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Download and install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Echo helm version
+helm version
